a1ac66ad3b8c942300388b09a46141918eb03ad6
Ludovicdevillelongue <ludovicdevillelongue@yahoo.fr>
2023-12-12 12:17:47 +0100

    optimization improvement and dash update

--- launcher/runner_strat.bat
+++ launcher/runner_strat.bat
@@ -1,3 +1,9 @@
 @echo off
-python C:\Users\ludov\Documents\pro\projets\trading_platform\launcher\runner_v1.py
+SET "CONDA_PATH=C:\Users\ludov\Anaconda3"
+SET "ENV_NAME=trading_platform"
+SET "SCRIPT_PATH=C:\Users\ludov\Documents\pro\projets\trading_platform\launcher\runner_v1.py"
+
+CALL "%CONDA_PATH%\Scripts\activate.bat" %CONDA_PATH%
+CALL conda activate %ENV_NAME%
+CALL python "%SCRIPT_PATH%"
 pause
