12de8c5f69cdef50bc03c817f9cb64bee9577046
Ludovicdevillelongue <ludovicdevillelongue@yahoo.fr>
2024-09-22 17:54:10 +0200

    Remove ignored files

--- portfolio_manager/runner_pm.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET "VENV_PATH=C:\Users\Admin\Documents\Pro\projets_code\python\trading_platform\.venv"
-SET "SCRIPT_PATH=C:\Users\Admin\Documents\Pro\projets_code\python\trading_platform\portfolio_manager\portfolio_dash.py"
-
-CALL "%VENV_PATH%\Scripts\activate.bat"
-CALL "%VENV_PATH%\Scripts\python.exe" "%SCRIPT_PATH%" error_log.txt 2>&1 || pause
-exit 0
